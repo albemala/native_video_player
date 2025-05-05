78621aaae74cd02c711b4f44907271381dd0c367
albemala <albemala@gmail.com>
2025-05-05 18:43:32 +0200

    updated example files

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import native_video_player
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NativeVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "NativeVideoPlayerPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
